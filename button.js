16876e9fe0fd41f46ddc9f7ddcf5d2037790e715
黄利蓉 <huanglirong@nbtide.net>
2023-01-17 15:59:39 +0800

    add h-button and style

---
+++ button.js
@@ -0,0 +1,10 @@
+/**
+ * @Description:button
+ * @Author: hlr
+ * @Date:2023-01-17 15:37
+ * @LastEditors: hlr
+ * @LastEditTime: 2023-01-17 15:37
+ */
+Vue.component('h-button',{
+    template:`<button class="h-button">hlr</button>`
+})
