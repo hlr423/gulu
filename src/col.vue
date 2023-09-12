600cbbe54f489f3abc58a194a8a303ee9342dac4
hlr423 <1217402420@qq.com>
2023-09-12 15:08:17 +0800

    row和col重构

--- src/col.vue
+++ src/col.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="col" :class="[`col-${span}`]">
+    <div class="col" :class="colClass" :style="colStyle">
         <slot></slot>
     </div>
 </template>
@@ -10,6 +10,32 @@
         props:{
             span:{
                 type:[Number,String]
+            },
+            offset:{
+                type:[Number,String]
+            }
+        },
+        data(){
+            return{
+                gutter:{
+                    type:[Number,String],
+                    default:0
+                }
+            }
+        },
+        computed:{
+            colClass(){
+                let {span,offset}=this;
+                return [
+                    span && `col-${span}`,
+                    offset && `offset-${offset}`
+                ]
+            },
+            colStyle(){
+               return{
+                   paddingLeft:this.gutter && this.gutter/2+'px',
+                   paddingRight:this.gutter && this.gutter/2+'px'
+               }
             }
         }
     }
@@ -17,14 +43,18 @@
 
 <style scoped lang="scss">
     .col{
-        height: 100px;
-        background-color: dodgerblue;
+        /*background-color: dodgerblue;*/
         width: 50%;
-        border: 1px solid #22BFE4;
         $class-prefix:col-;
         @for $n from 1 through 24{
             &.#{$class-prefix}#{$n} {
                 width:($n / 24) * 100%;
+            }
+        }
+        $class-prefix:offset-;
+        @for $n from 1 through 24{
+            &.#{$class-prefix}#{$n} {
+                margin-left:($n / 24) * 100%;
             }
         }
     }
--- src/row.vue
+++ src/row.vue
@@ -1,17 +1,38 @@
 <template>
-    <div class="row">
+    <div class="row" :style="rowStyle">
         <slot></slot>
     </div>
 </template>
 
 <script>
     export default {
-        name: "row"
+        name: "h-row",
+        props:{
+            gutter:{
+                type:[Number,String]
+            }
+        },
+        computed:{
+            rowStyle(){
+                return{
+                    marginLeft:this.gutter && -this.gutter/2+'px',
+                    marginRight:this.gutter && -this.gutter/2+'px'
+                }
+            }
+        },
+        mounted() {
+            this.$children.forEach(vm=>{
+                vm.gutter=this.gutter
+            })
+        }
     }
+//
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .row{
     display: flex;
+    /*background-color: #bbbbbb;*/
+    overflow: hidden;
 }
 </style>
